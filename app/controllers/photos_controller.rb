fea2112f93076268fa07308bc3dad86958f33ba1
eqiu23 <eqiu@chicagobooth.edu>
2022-05-09 16:28:59 +0000

    Photos details looks good + delete works

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -6,4 +6,18 @@ class PhotosController < ApplicationController
     @list_of_photos = matching_photos.order({ :created_at => :desc })
     render({ :template => "photo_templates/index.html.erb" }) 
   end
+
+  def show
+    matching_photos = Photo.where({ :id => params.fetch("path_id")})
+    @the_photo = matching_photos.at(0)
+    render({ :template => "photo_templates/show.html.erb" }) 
+  end
+
+  def delete
+    matching_photos = Photo.where({ :id => params.fetch("path_id") })
+    the_photo = matching_photos.at(0)
+    the_photo.destroy
+    # render({ :template => "photo_templates/delete.html.erb" }) 
+    redirect_to("/photos")
+  end
 end
